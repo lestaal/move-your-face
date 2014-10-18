8757fdf789ff8a0b236ef6ff6732e179499fe17b
Lauren Staal <les6@rice.edu>
2014-10-18 16:54:43 -0500

    removed overlay messages

--- face.js
+++ face.js
@@ -3,7 +3,7 @@ function init() {
 	var videoInput = document.getElementById('inputVideo');
 	var canvasInput = document.getElementById('inputCanvas');
 
-	var htracker = new headtrackr.Tracker();
+	var htracker = new headtrackr.Tracker({ui : false});
 	htracker.init(videoInput, canvasInput);
 	htracker.start();
 }
